6e1cc797384e78dda38b38ba0671e07ad3bd264e
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2023-04-02 17:06:57 +0000

    Check or correct exception

--- tests/COARNotificationManagerTest.php
+++ tests/COARNotificationManagerTest.php
@@ -3,14 +3,14 @@
 use PHPUnit\Framework\TestCase;
 use Http\Mock\Client;
 use cottagelabs\coarNotifications\COARNotificationManager;
-use cottagelabs\coarNotifications\orm\COARNotificationException;
+use cottagelabs\coarNotifications\orm\COARNotificationNoDatabaseException;
 
 final class COARNotificationManagerTest extends TestCase
 {
 
     public function testNeedsValidConnection(): void
     {
-        $this->expectException(COARNotificationException::class);
+        $this->expectException(COARNotificationNoDatabaseException::class);
 
         new COARNotificationManager("invalid connection");
     }
